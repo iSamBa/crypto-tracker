e0715fc24901d2bd9f89a7ae0bf86ed1f8eb56cb
iSamBa <bahou.aissam@gmail.com>
2021-04-18 17:02:54 +0200

    add navbar component

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,10 +1,39 @@
 <template>
-  <div></div>
+  <nav class="navbar navbar-expand-lg navbarlight box-inner">
+    <nuxt-link to="/" exact>
+      <span class="logo">MyLogo</span>
+    </nuxt-link>
+    <button
+      class="navbar-toggler collapsed"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarsExampleDefault"
+      aria-controls="navbarsExampleDefault"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="icon-bar top-bar" />
+      <span class="icon-bar middle-bar" />
+      <span class="icon-bar bottom-bar" />
+    </button>
+
+    <div id="navbarsExampleDefault" class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <nav-link link="index">Home</nav-link>
+        <nav-link link="about">About</nav-link>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
   name: 'NavBar',
+  watch: {
+    $route() {
+      $('navbarsExampleDefault').collapse('hide')
+    },
+  },
 }
 </script>
 
