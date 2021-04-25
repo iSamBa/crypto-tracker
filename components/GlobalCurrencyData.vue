c93944217b08f094878798ac615512d1ac5aeed8
iSamBa <bahou.aissam@gmail.com>
2021-04-25 14:45:49 +0200

    fix data form for global market data

--- components/GlobalCurrencyData.vue
+++ components/GlobalCurrencyData.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="global-market">
     <div class="total global-market-item">
-      <h4>Total Market volume</h4>
+      <h4 class="title">TOTAL MARKET VOLUME</h4>
       <span v-if="globalData.total_volume">{{
         globalData.total_volume.eur
       }}</span>
     </div>
     <div class="capacity global-market-item">
-      <h4>Market capacity %</h4>
-      <div v-if="marketCapacityPercentage">
-        <ul>
-          <li>btc : {{ marketCapacityPercentage.btc }} %</li>
-          <li>eth : {{ marketCapacityPercentage.eth }} %</li>
-          <li>bnb : {{ marketCapacityPercentage.bnb }} %</li>
-        </ul>
+      <h4 class="title">MARKET CAPACITY</h4>
+      <div v-if="marketCapacityPercentage" class="market-capacity">
+        <div class="market-capacity-item">
+          <p>BTC</p>
+          <p>{{ marketCapacityPercentageBtc }} <span>%</span></p>
+        </div>
+        <div class="market-capacity-item">
+          <p>ETH</p>
+          <p>{{ marketCapacityPercentageEth }} <span>%</span></p>
+        </div>
+        <div class="market-capacity-item">
+          <p>BNB</p>
+          <p>{{ marketCapacityPercentageBnb }} <span>%</span></p>
+        </div>
       </div>
     </div>
     <div class="active-currencies global-market-item">
-      <h4>Active crypto currencies</h4>
+      <h4 class="title">ACTIVE CRYPTOCURRENCIES</h4>
       <span v-if="globalData.active_cryptocurrencies">{{
         globalData.active_cryptocurrencies
       }}</span>
@@ -33,6 +40,15 @@ export default {
     },
     marketCapacityPercentage() {
       return this.globalData.market_cap_percentage
+    },
+    marketCapacityPercentageBtc() {
+      return this.globalData.market_cap_percentage.btc.toFixed(2)
+    },
+    marketCapacityPercentageEth() {
+      return this.globalData.market_cap_percentage.eth.toFixed(2)
+    },
+    marketCapacityPercentageBnb() {
+      return this.globalData.market_cap_percentage.bnb.toFixed(2)
     },
   },
   async mounted() {
