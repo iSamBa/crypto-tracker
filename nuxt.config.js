13ee082b143ce4a8fd1c3f9414751d4d357e96ef
iSamBa <bahou.aissam@gmail.com>
2021-04-29 17:43:54 +0200

    install vue-good-table component

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,10 @@ export default {
   css: ['@/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/chartkick.js'],
+  plugins: [
+    '~/plugins/chartkick.js',
+    { src: '~/plugins/vue-good-table', ssr: false },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
