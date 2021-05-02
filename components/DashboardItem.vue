b12234a0f9456298fe0bb9dad237d09a48868121
iSamBa <bahou.aissam@gmail.com>
2021-05-03 00:20:34 +0200

    build first dummy example using vue-chartjs

--- components/DashboardItem.vue
+++ components/DashboardItem.vue
@@ -1,10 +1,48 @@
 <template>
   <div>
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
     <global-currency-data></global-currency-data>
   </div>
 </template>
 
 <script>
-export default {}
+import LineChart from './LineChart.js'
+
+export default {
+  components: {
+    LineChart,
+  },
+  data() {
+    return {
+      datacollection: null,
+    }
+  },
+  mounted() {
+    this.fillData()
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()],
+          },
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()],
+          },
+        ],
+      }
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    },
+  },
+}
 </script>
 <style lang="scss" scoped></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    // transpile: ['chartkick'],
-  },
+  build: {},
 }
